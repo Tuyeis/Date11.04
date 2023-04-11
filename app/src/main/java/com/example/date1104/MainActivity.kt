e1f167749d8cf3d0134db8eba9512227fc0649df
pnietopaez <pablonp67@gmail.com>
2023-04-11 10:40:03 +0200

    fertig

--- app/src/main/java/com/example/date1104/MainActivity.kt
+++ app/src/main/java/com/example/date1104/MainActivity.kt
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import com.example.date1104.databinding.ActivityMainBinding
+import org.json.JSONArray
+import org.json.JSONObject
 import java.io.BufferedReader
 import java.io.InputStreamReader
 
@@ -27,6 +29,74 @@ class MainActivity : AppCompatActivity() {
              bufferedReader.readText()
          }
         Log.d(TAG, "onCreate: JsonString -> $jsonString")
+
+        val rootObject = JSONObject(jsonString)
+        val userArrayJson: JSONArray
+        if(rootObject.has(USERS))
+            userArrayJson = rootObject.getJSONArray(USERS)
+        else
+            userArrayJson = JSONArray()
+        for(index in 0 until userArrayJson.length())
+        {
+            var name : String? = null
+            var job : String? =null
+            var age: Int? = null
+            var city: String? = null
+            var hobbys: ArrayList<String?> = ArrayList()
+            var PhonePrivate: String? = null
+            var PhoneOffice: String? = null
+            var PhoneMobile: String? = null
+
+            val userJson = userArrayJson.getJSONObject(index)
+
+            if(userJson.has(NAME))
+            name = userJson.getString(NAME)
+            if(userJson.has(JOB))
+                job = userJson.getString(JOB)
+            if(userJson.has(AGE))
+                age = userJson.getInt(AGE)
+            if(userJson.has(CITY))
+                city = userJson.getString(CITY)
+            if(userJson.has(HOBBYS)){
+                val hobbyArray = userJson.getJSONArray(HOBBYS)
+                for(indexHobby in 0 until hobbyArray.length()){
+                    hobbys.add(hobbyArray.getString(indexHobby))
+
+                }
+            }
+            if(userJson.has(PHONE)){
+                val phoneUser= userJson.getJSONObject(PHONE)
+
+                if(phoneUser.has(PRIVATE))
+                    PhonePrivate=phoneUser.getString(PRIVATE)
+                if(phoneUser.has(OFFICE))
+                    PhoneOffice=phoneUser.getString(OFFICE)
+                if(phoneUser.has(MOBILE))
+                    PhoneMobile=phoneUser.getString(MOBILE)
+
+            }
+
+            val user = User(name,job,age,hobbys,Phone(PhonePrivate,PhoneOffice,PhoneMobile))
+            userList.add(user)
+
+            userList.forEach{
+                binding.tvOutput.append("\n$it")
+            }
+
+
+        }
+    }
+    companion object{
+        val USERS = "users"
+        val NAME = "name"
+        val JOB = "job"
+        val AGE = "age"
+        val CITY = "city"
+        val HOBBYS = "hobbys"
+        val  PHONE = "phone"
+        val PRIVATE = "private"
+        val OFFICE = "OFFICE"
+        val MOBILE = "mobile"
 
     }
 }
--- app/src/main/java/com/example/date1104/User.kt
+++ app/src/main/java/com/example/date1104/User.kt
@@ -5,7 +5,7 @@ package com.example.date1104
 data class User (
     val name: String?,
     val job : String?,
-    val age : Int,
+    val age : Int?,
     val hobbys : ArrayList<String?>,
     val phone : Phone?
         )
